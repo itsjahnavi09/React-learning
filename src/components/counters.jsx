15db37c672fa26de451cc6024f2591d91c9ec762
Jahnavi <jahnavi@Jahnavis-MacBook-Pro.local>
2026-06-17 12:11:47 +0530

    added counters

---
+++ src/components/counters.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "../App.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter-container">
+      <h1>Counter </h1>
+
+      <h2>Count: {count}</h2>
+
+      <div className="button-group">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
